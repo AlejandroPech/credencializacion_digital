e5ffd6907808f690724a041701f4c235336e028c
JAXA0102 <jared777.j7@gmail.com>
2021-08-02 02:19:14 -0500

    Empresas Jared

--- lib/src/pages/empresas_page.dart
+++ lib/src/pages/empresas_page.dart
@@ -2,98 +2,118 @@ import 'package:credencializacion_digital/src/pages/empresa_cupon_page.dart';
 import 'package:credencializacion_digital/src/pages/empresa_cupones_page.dart';
 import 'package:credencializacion_digital/src/theme/theme.dart';
 import 'package:credencializacion_digital/src/widgets/menu_widget.dart';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:meta/meta.dart';
+import 'package:credencializacion_digital/src/models/Empresas.dart';
+import 'package:dio/dio.dart';
+
 
 class EmpresaPage extends StatefulWidget {
-  static final String routeName='empresa';
+  static final String routeName = 'empresa';
   final String title;
 
-  EmpresaPage({Key key,@required this.title}) : super(key: key);
+  EmpresaPage({Key key, @required this.title}) : super(key: key);
 
   @override
   _EmpresaPageState createState() => _EmpresaPageState();
 }
 
 class _EmpresaPageState extends State<EmpresaPage> {
+  final String baseurl = "http://10.0.2.2:8000/api/Empresas";
+
+  void _dataFron {
+    final Dio dio = new Dio();
+  }
+
   @override
   Widget build(BuildContext context) {
     bool isLarge;
 
-    if(MediaQuery.of(context).size.width>=500){
-      isLarge=true;
-    }else{
-      isLarge=false;
+    if (MediaQuery.of(context).size.width >= 500) {
+      isLarge = true;
+    } else {
+      isLarge = false;
     }
 
-    final appTheme= Provider.of<ThemeChanger>(context);
+    final appTheme = Provider.of<ThemeChanger>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       drawer: MenuWidget(),
       body: GridView.count(
-        crossAxisCount: (isLarge)?3:2,
+        crossAxisCount: (isLarge) ? 3 : 2,
         children: [
           Carditem(
-              image: "https://media-cdn.tripadvisor.com/media/photo-s/08/af/e2/d3/distrito-capital-federal.jpg",
-              color: appTheme.currentTheme.accentColor,
-              urlNavegar: EmpresaCuponPage.routeName,
+            image:
+                "https://media-cdn.tripadvisor.com/media/photo-s/08/af/e2/d3/distrito-capital-federal.jpg",
+            color: appTheme.currentTheme.accentColor,
+            urlNavegar: EmpresaCuponPage.routeName,
           ),
           Carditem(
-            image: "https://www.america-retail.com/static//2020/08/Noticia-5-1-de-3-339.png",
+            image:
+                "https://www.america-retail.com/static//2020/08/Noticia-5-1-de-3-339.png",
             color: appTheme.currentTheme.accentColor,
             urlNavegar: EmpresaCuponesPage.routeName,
           ),
           Carditem(
-              image: "https://media-cdn.tripadvisor.com/media/photo-s/08/af/e2/d3/distrito-capital-federal.jpg",
-              color: appTheme.currentTheme.accentColor,
-              urlNavegar: EmpresaCuponPage.routeName,
-          ),
-          Carditem(
-            image: "https://www.peru-retail.com/wp-content/uploads/Little-Caesars-1.jpg",
+            image:
+                "https://media-cdn.tripadvisor.com/media/photo-s/08/af/e2/d3/distrito-capital-federal.jpg",
             color: appTheme.currentTheme.accentColor,
             urlNavegar: EmpresaCuponPage.routeName,
           ),
           Carditem(
-              image: "https://media-cdn.tripadvisor.com/media/photo-s/08/af/e2/d3/distrito-capital-federal.jpg",
-              color: appTheme.currentTheme.accentColor,
-              urlNavegar: EmpresaCuponPage.routeName,
+            image:
+                "https://www.peru-retail.com/wp-content/uploads/Little-Caesars-1.jpg",
+            color: appTheme.currentTheme.accentColor,
+            urlNavegar: EmpresaCuponPage.routeName,
           ),
           Carditem(
-            image: "https://www.america-retail.com/static//2020/08/Noticia-5-1-de-3-339.png",
+            image:
+                "https://media-cdn.tripadvisor.com/media/photo-s/08/af/e2/d3/distrito-capital-federal.jpg",
+            color: appTheme.currentTheme.accentColor,
+            urlNavegar: EmpresaCuponPage.routeName,
+          ),
+          Carditem(
+            image:
+                "https://www.america-retail.com/static//2020/08/Noticia-5-1-de-3-339.png",
             color: appTheme.currentTheme.accentColor,
             urlNavegar: EmpresaCuponesPage.routeName,
           ),
           Carditem(
-              image: "https://media-cdn.tripadvisor.com/media/photo-s/08/af/e2/d3/distrito-capital-federal.jpg",
-              color: appTheme.currentTheme.accentColor,
-              urlNavegar: EmpresaCuponPage.routeName,
-          ),
-          Carditem(
-            image: "https://www.peru-retail.com/wp-content/uploads/Little-Caesars-1.jpg",
+            image:
+                "https://media-cdn.tripadvisor.com/media/photo-s/08/af/e2/d3/distrito-capital-federal.jpg",
             color: appTheme.currentTheme.accentColor,
             urlNavegar: EmpresaCuponPage.routeName,
           ),
           Carditem(
-              image: "https://media-cdn.tripadvisor.com/media/photo-s/08/af/e2/d3/distrito-capital-federal.jpg",
-              color: appTheme.currentTheme.accentColor,
-              urlNavegar: EmpresaCuponesPage.routeName,
-          ),
-          Carditem(
-            image: "https://www.america-retail.com/static//2020/08/Noticia-5-1-de-3-339.png",
+            image:
+                "https://www.peru-retail.com/wp-content/uploads/Little-Caesars-1.jpg",
             color: appTheme.currentTheme.accentColor,
             urlNavegar: EmpresaCuponPage.routeName,
           ),
           Carditem(
-              image: "https://media-cdn.tripadvisor.com/media/photo-s/08/af/e2/d3/distrito-capital-federal.jpg",
-              color: appTheme.currentTheme.accentColor,
-              urlNavegar: EmpresaCuponPage.routeName,
+            image:
+                "https://media-cdn.tripadvisor.com/media/photo-s/08/af/e2/d3/distrito-capital-federal.jpg",
+            color: appTheme.currentTheme.accentColor,
+            urlNavegar: EmpresaCuponesPage.routeName,
           ),
           Carditem(
-            image: "https://www.peru-retail.com/wp-content/uploads/Little-Caesars-1.jpg",
+            image:
+                "https://www.america-retail.com/static//2020/08/Noticia-5-1-de-3-339.png",
+            color: appTheme.currentTheme.accentColor,
+            urlNavegar: EmpresaCuponPage.routeName,
+          ),
+          Carditem(
+            image:
+                "https://media-cdn.tripadvisor.com/media/photo-s/08/af/e2/d3/distrito-capital-federal.jpg",
+            color: appTheme.currentTheme.accentColor,
+            urlNavegar: EmpresaCuponPage.routeName,
+          ),
+          Carditem(
+            image:
+                "https://www.peru-retail.com/wp-content/uploads/Little-Caesars-1.jpg",
             color: appTheme.currentTheme.accentColor,
             urlNavegar: EmpresaCuponPage.routeName,
           ),
@@ -107,24 +127,30 @@ class Carditem extends StatelessWidget {
   final String image;
   final Color color;
   final String urlNavegar;
-  const Carditem({@required this.image, @required this.color,@required this.urlNavegar});
+  const Carditem(
+      {@required this.image, @required this.color, @required this.urlNavegar});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Center(
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                child: Image.network(this.image,fit: BoxFit.cover,),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Image.network(
+                this.image,
+                fit: BoxFit.cover,
               ),
-              
-              TextButton(
-                onPressed:()=>Navigator.pushNamed(context, urlNavegar),
-                child: Text('Ver Cupones',style: TextStyle(color: this.color,fontSize: 18),),
-              )
-            ],
-          ),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pushNamed(context, urlNavegar),
+              child: Text(
+                'Ver Cupones',
+                style: TextStyle(color: this.color, fontSize: 18),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
